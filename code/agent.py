02dcca5e3fce1bd9103ed84fcf9607d8289c1a39
tommipoiko <tommi.poiko@gmail.com>
2023-10-22 14:14:53 +0300

    Needs work on PM instructions and function call handling

--- code/agent.py
+++ code/agent.py
@@ -1,3 +1,5 @@
+import json
+
 from openai import ChatCompletion
 import openai
 import secret_variables as sv
@@ -12,11 +14,31 @@ class GPTAgent:
             self.message_history = [{"role": "system", "content": description}]
         else:
             self.message_history = []
+        self.functions = [
+            {
+                "name": "open_file",
+                "description": "Open a file and write the content to it.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "file_name": {
+                            "type": "string",
+                            "description": "The name of the file to open."
+                        },
+                        "content": {
+                            "type": "string",
+                            "description": "The content to write to the file."
+                        }
+                    },
+                    "required": ["file_name", "content"]
+                }
+            }
+        ]
 
-    @staticmethod
-    def _handle_response(response) -> str:
+    def _handle_response_chunks(self, response) -> str:
+        # Remember to set stream=True to use this function.
         collected_messages = []
-        print("Response:\n")
+        print(f"\nResponse from {self.role}:\n")
         for chunk in response:
             try:
                 chunk_message = chunk["choices"][0]["delta"]["content"]
@@ -25,13 +47,74 @@ class GPTAgent:
             except KeyError:
                 pass
         print("\n")
-
         return "".join(collected_messages).strip()
+
+    def _handle_response(self, response) -> str:
+        response_message = response["choices"][0]["message"]
+        print(f"\nResponse from {self.role}:\n")
+        print(response_message["content"].strip())
+        print("\n")
+        return response_message["content"].strip()
+
+    def _handle_function_calls(self, response) -> None | dict:
+        response_message = response["choices"][0]["message"]
+        if response_message.get("function_call"):
+            available_functions = {
+                "open_file": self.open_file
+            }
+            function_name = response_message["function_call"]["name"]
+            function_to_call = available_functions[function_name]
+            function_args = json.loads(response_message["function_call"]["arguments"])
+            function_response = function_to_call(
+                file_name=function_args["file_name"],
+                content=function_args["content"]
+            )
+            return {"role": "function", "name": function_name, "content": function_response}
+        else:
+            return None
+
+    def _handle_response_and_function_calls(self, response) -> dict:
+        response_message = response["choices"][0]["message"]
+
+        # TODO Check what the response looks like when the agent calls a function and then sends a
+        #  message.
+        print(response["choices"])
+
+        if response_message.get("function_call"):
+            available_functions = {
+                "open_file": self.open_file
+            }
+            function_name = response_message["function_call"]["name"]
+            function_to_call = available_functions[function_name]
+            function_args = json.loads(response_message["function_call"]["arguments"])
+            function_response = function_to_call(
+                file_name=function_args["file_name"],
+                content=function_args["content"]
+            )
+            return {"role": "function", "name": function_name, "content": f"function_response"}
+        else:
+            print(f"\nResponse from {self.role}:\n")
+            print(response_message["content"].strip() + "\n")
+            return {"role": "assistant", "content": response_message["content"].strip()}
 
     def communicate(self, message: str) -> str:
         self.message_history.append({"role": "user", "content": message})
-        response = ChatCompletion.create(model="gpt-4", messages=self.message_history, stream=True)
-        full_reply_content = self._handle_response(response)
-        self.message_history.append({"role": "assistant", "content": full_reply_content})
+        response = ChatCompletion.create(model="gpt-4", messages=self.message_history,
+                                         functions=self.functions, function_call="auto")
+        full_reply_content = self._handle_response_and_function_calls(response)
+        self.message_history.append(full_reply_content)
+        if full_reply_content["role"] == "function":
+            return "Function called"
+        else:
+            return full_reply_content["content"]
 
-        return full_reply_content
+    @staticmethod
+    def open_file(file_name: str, content: str) -> bool:
+        """Open a file and write the content to it."""
+        try:
+            with open(f"agent_files/{file_name}", "w+") as f:
+                f.write(content)
+                return True
+        except Exception as e:
+            print(e)
+            return False
--- code/multiple_agents.py
+++ code/multiple_agents.py
@@ -18,23 +18,25 @@ AGENTS = {
                                 "infrastructure, ensures data is clean, accessible, and "
                                 "optimized for machine learning tasks. You will be working "
                                 "with the team you proposed and to contact one of them, "
-                                "you will start the response with their title, followed "
-                                "by two newlines. To say that the project is done, you "
-                                "will start your response with 'The project has been completed!'. "
-                                "Do not include Project Manager in the listing, as that is your "
-                                "job. The previous example of a Data Engineer is just an example, "
-                                "a Data Engineer might be different, or not needed at all. When I "
-                                "accept the team, you will start to address different members of "
-                                "your team by providing their title at the start of the prompt. "
-                                "Only address one team member at a time! If the team member wishes "
-                                "to speak to some other member, you will pass them the message! "
-                                "To ask me questions, start the response with 'Hello client!'. "
-                                "First, tell all other team members about the project and ask them "
-                                "to answer only 'Understood', then communicate with the team "
-                                "members to move the project forward. Remember to tell team "
-                                "members about things the previous team members have done! When "
-                                "the project has been completed. Also provide me the results of "
-                                "the project!"),
+                                "you will start the response with ONLY their title AND NOTHING ELSE"
+                                "(DO NOT USE COMMAS OR ANYTHING!!!), followed by two newlines. To "
+                                "say that the project is done, you will start your response with "
+                                "'The project has been completed!'. Do not include Project Manager "
+                                "in the listing, as that is your job. The previous example of a "
+                                "Data Engineer is just an example, a Data Engineer might be "
+                                "different, or not needed at all. When I accept the team, you will "
+                                "start to address different members of your team by providing their"
+                                " title at the start of the prompt. Only address one team member at"
+                                " a time! If the team member wishes to speak to some other member, "
+                                "you will pass them the message! To ask me questions, start the "
+                                "response with 'Hello client!'. First, tell all other team members "
+                                "about the project and ask them to answer only 'Understood', then "
+                                "communicate with the team members to move the project forward. "
+                                "Remember to tell team members about things the previous team "
+                                "members have done! When the project has been completed. Also "
+                                "provide me the results of the project! If I tell you to drop a "
+                                "team member, you will just do it and not acknowledge it by saying "
+                                "anything about it.")
 }
 
 EXIT_COMMANDS = ["q", "quit", "e", "exit", "s", "stop"]
@@ -42,43 +44,35 @@ EXIT_COMMANDS = ["q", "quit", "e", "exit", "s", "stop"]
 
 def main():
     prompt = input("Idea:\n\n")
-
-    if prompt in EXIT_COMMANDS:
+    if prompt in EXIT_COMMANDS or not prompt.strip():
         return
-
     team = AGENTS["Project Manager"].communicate(prompt)
     team_list = [tm for tm in team.split("\n") if tm]
-
     for tm in team_list:
         tm_object = tm.split("***")
         title = tm_object[0].strip()
         skills = tm_object[1].strip()
         role = tm_object[2].strip()
         AGENTS[title] = GPTAgent(title, f"Role in the team: {role}. Skills: {skills}")
-
     prompt = input("Prompt:\n\n")
-
     while prompt.lower() not in EXIT_COMMANDS:
         response = AGENTS["Project Manager"].communicate(prompt)
         lines = response.split("\n")
         agent_name = lines[0].strip()
         agent_message = "\n".join(lines[1:]).strip()
-        
         if agent_name == "Hello client!":
             prompt = input("Prompt:\n\n")
         elif agent_name == "The project has been completed!":
             return
         else:
             prompt = AGENTS[agent_name].communicate(agent_message)
-
-        sleep(5)
+        sleep(10)
 
 
 if __name__ == "__main__":
     main()
 
 """
-The team members do not know where we are at with the development. Either send the chat history
-to each member (probably not viable) or make sure the Project Manager let's the other people know
-where we are at.
+Give the Project Manager better instructions on how to communicate with the team members and how
+to proceed with the planning and execution of this project.
 """
